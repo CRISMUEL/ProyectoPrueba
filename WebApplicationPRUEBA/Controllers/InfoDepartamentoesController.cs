acbbe212904940b4515c33b68b84714a8a43bd68
CRISMUEL <cristiansfeliz1871@gmail.com>
2019-12-03 23:39:16 -0500

    Agregando los informes de los depa fase beta

---
+++ WebApplicationPRUEBA/Controllers/InfoDepartamentoesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationPRUEBA.Models;
+
+namespace WebApplicationPRUEBA.Controllers
+{
+    public class InfoDepartamentoesController : Controller
+    {
+        private RecursosHumanosEntities1 db = new RecursosHumanosEntities1();
+
+        // GET: InfoDepartamentoes
+        public ActionResult Index()
+        {
+            return View(db.Departamentoes.ToList());
+        }
+
+        // GET: InfoDepartamentoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Departamento departamento = db.Departamentoes.Find(id);
+            if (departamento == null)
+            {
+                return HttpNotFound();
+            }
+            return View(departamento);
+        }
+
+        // GET: InfoDepartamentoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: InfoDepartamentoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,codigoDepartamento,nombre,idManager")] Departamento departamento)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Departamentoes.Add(departamento);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(departamento);
+        }
+
+        // GET: InfoDepartamentoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Departamento departamento = db.Departamentoes.Find(id);
+            if (departamento == null)
+            {
+                return HttpNotFound();
+            }
+            return View(departamento);
+        }
+
+        // POST: InfoDepartamentoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,codigoDepartamento,nombre,idManager")] Departamento departamento)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(departamento).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(departamento);
+        }
+
+        // GET: InfoDepartamentoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Departamento departamento = db.Departamentoes.Find(id);
+            if (departamento == null)
+            {
+                return HttpNotFound();
+            }
+            return View(departamento);
+        }
+
+        // POST: InfoDepartamentoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Departamento departamento = db.Departamentoes.Find(id);
+            db.Departamentoes.Remove(departamento);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
