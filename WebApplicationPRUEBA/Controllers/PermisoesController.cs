097c6bc9ed995dc097685f622a394eced60ffa9b
CRISMUEL <cristiansfeliz1871@gmail.com>
2019-12-03 19:48:39 -0500

    Creando los permisos y licencias

---
+++ WebApplicationPRUEBA/Controllers/PermisoesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationPRUEBA.Models;
+
+namespace WebApplicationPRUEBA.Controllers
+{
+    public class PermisoesController : Controller
+    {
+        private RecursosHumanosEntities db = new RecursosHumanosEntities();
+
+        // GET: Permisoes
+        public ActionResult Index()
+        {
+            return View(db.Permisos.ToList());
+        }
+
+        // GET: Permisoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Permiso permiso = db.Permisos.Find(id);
+            if (permiso == null)
+            {
+                return HttpNotFound();
+            }
+            return View(permiso);
+        }
+
+        // GET: Permisoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Permisoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,idEmpleado,desde,hasta,comentarios")] Permiso permiso)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Permisos.Add(permiso);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(permiso);
+        }
+
+        // GET: Permisoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Permiso permiso = db.Permisos.Find(id);
+            if (permiso == null)
+            {
+                return HttpNotFound();
+            }
+            return View(permiso);
+        }
+
+        // POST: Permisoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,idEmpleado,desde,hasta,comentarios")] Permiso permiso)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(permiso).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(permiso);
+        }
+
+        // GET: Permisoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Permiso permiso = db.Permisos.Find(id);
+            if (permiso == null)
+            {
+                return HttpNotFound();
+            }
+            return View(permiso);
+        }
+
+        // POST: Permisoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Permiso permiso = db.Permisos.Find(id);
+            db.Permisos.Remove(permiso);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
