c3f729e93fd560354d53fd94cabf570db9c2b4d5
CRISMUEL <cristiansfeliz1871@gmail.com>
2019-12-04 00:39:30 -0500

    Agregando InfoSalidas

--- WebApplicationPRUEBA/Controllers/InfoSalidasController.cs
+++ WebApplicationPRUEBA/Controllers/InfoSalidasController.cs
@@ -10,107 +10,107 @@ using WebApplicationPRUEBA.Models;
 
 namespace WebApplicationPRUEBA.Controllers
 {
-    public class salidasMes_ResultController : Controller
+    public class InfoSalidasController : Controller
     {
         private RecursosHumanosEntities1 db = new RecursosHumanosEntities1();
 
-        // GET: salidasMes_Result
+        // GET: InfoSalidas
         public ActionResult Index()
         {
-            return View(db.salidasMes_Result.ToList());
+            return View(db.Salidas.ToList());
         }
 
-        // GET: salidasMes_Result/Details/5
+        // GET: InfoSalidas/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            salidasMes_Result salidasMes_Result = db.salidasMes_Result.Find(id);
-            if (salidasMes_Result == null)
+            Salida salida = db.Salidas.Find(id);
+            if (salida == null)
             {
                 return HttpNotFound();
             }
-            return View(salidasMes_Result);
+            return View(salida);
         }
 
-        // GET: salidasMes_Result/Create
+        // GET: InfoSalidas/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: salidasMes_Result/Create
+        // POST: InfoSalidas/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "id,Empleado,idEmpleado,tipoSalida,motivo,fechaSalida")] salidasMes_Result salidasMes_Result)
+        public ActionResult Create([Bind(Include = "id,idEmpleado,tipoSalida,motivo,fechaSalida")] Salida salida)
         {
             if (ModelState.IsValid)
             {
-                db.salidasMes_Result.Add(salidasMes_Result);
+                db.Salidas.Add(salida);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            return View(salidasMes_Result);
+            return View(salida);
         }
 
-        // GET: salidasMes_Result/Edit/5
+        // GET: InfoSalidas/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            salidasMes_Result salidasMes_Result = db.salidasMes_Result.Find(id);
-            if (salidasMes_Result == null)
+            Salida salida = db.Salidas.Find(id);
+            if (salida == null)
             {
                 return HttpNotFound();
             }
-            return View(salidasMes_Result);
+            return View(salida);
         }
 
-        // POST: salidasMes_Result/Edit/5
+        // POST: InfoSalidas/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "id,Empleado,idEmpleado,tipoSalida,motivo,fechaSalida")] salidasMes_Result salidasMes_Result)
+        public ActionResult Edit([Bind(Include = "id,idEmpleado,tipoSalida,motivo,fechaSalida")] Salida salida)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(salidasMes_Result).State = EntityState.Modified;
+                db.Entry(salida).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(salidasMes_Result);
+            return View(salida);
         }
 
-        // GET: salidasMes_Result/Delete/5
+        // GET: InfoSalidas/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            salidasMes_Result salidasMes_Result = db.salidasMes_Result.Find(id);
-            if (salidasMes_Result == null)
+            Salida salida = db.Salidas.Find(id);
+            if (salida == null)
             {
                 return HttpNotFound();
             }
-            return View(salidasMes_Result);
+            return View(salida);
         }
 
-        // POST: salidasMes_Result/Delete/5
+        // POST: InfoSalidas/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            salidasMes_Result salidasMes_Result = db.salidasMes_Result.Find(id);
-            db.salidasMes_Result.Remove(salidasMes_Result);
+            Salida salida = db.Salidas.Find(id);
+            db.Salidas.Remove(salida);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
