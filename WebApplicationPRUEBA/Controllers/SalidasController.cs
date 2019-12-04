6a7d59f99d2c9f49869493b1791d3a93b9939da0
CRISMUEL <cristiansfeliz1871@gmail.com>
2019-12-03 19:08:20 -0500

    Creando vacaciones y salida

---
+++ WebApplicationPRUEBA/Controllers/SalidasController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationPRUEBA.Models;
+
+namespace WebApplicationPRUEBA.Controllers
+{
+    public class SalidasController : Controller
+    {
+        private RecursosHumanosEntities db = new RecursosHumanosEntities();
+
+        // GET: Salidas
+        public ActionResult Index()
+        {
+            return View(db.Salidas.ToList());
+        }
+
+        // GET: Salidas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Salida salida = db.Salidas.Find(id);
+            if (salida == null)
+            {
+                return HttpNotFound();
+            }
+            return View(salida);
+        }
+
+        // GET: Salidas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Salidas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,idEmpleado,tipoSalida,motivo,fechaSalida")] Salida salida)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Salidas.Add(salida);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(salida);
+        }
+
+        // GET: Salidas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Salida salida = db.Salidas.Find(id);
+            if (salida == null)
+            {
+                return HttpNotFound();
+            }
+            return View(salida);
+        }
+
+        // POST: Salidas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,idEmpleado,tipoSalida,motivo,fechaSalida")] Salida salida)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(salida).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(salida);
+        }
+
+        // GET: Salidas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Salida salida = db.Salidas.Find(id);
+            if (salida == null)
+            {
+                return HttpNotFound();
+            }
+            return View(salida);
+        }
+
+        // POST: Salidas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Salida salida = db.Salidas.Find(id);
+            db.Salidas.Remove(salida);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplicationPRUEBA/Controllers/VacacionesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationPRUEBA.Models;
+
+namespace WebApplicationPRUEBA.Controllers
+{
+    public class VacacionesController : Controller
+    {
+        private RecursosHumanosEntities db = new RecursosHumanosEntities();
+
+        // GET: Vacaciones
+        public ActionResult Index()
+        {
+            var vacaciones = db.Vacaciones.Include(v => v.Empleado);
+            return View(vacaciones.ToList());
+        }
+
+        // GET: Vacaciones/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Vacacione vacacione = db.Vacaciones.Find(id);
+            if (vacacione == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vacacione);
+        }
+
+        // GET: Vacaciones/Create
+        public ActionResult Create()
+        {
+            ViewBag.idEmpleado = new SelectList(db.Empleados, "id", "codigoEmpleado");
+            return View();
+        }
+
+        // POST: Vacaciones/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,idEmpleado,desde,hasta,correspondiente,comentarios")] Vacacione vacacione)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Vacaciones.Add(vacacione);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idEmpleado = new SelectList(db.Empleados, "id", "codigoEmpleado", vacacione.idEmpleado);
+            return View(vacacione);
+        }
+
+        // GET: Vacaciones/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Vacacione vacacione = db.Vacaciones.Find(id);
+            if (vacacione == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idEmpleado = new SelectList(db.Empleados, "id", "codigoEmpleado", vacacione.idEmpleado);
+            return View(vacacione);
+        }
+
+        // POST: Vacaciones/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,idEmpleado,desde,hasta,correspondiente,comentarios")] Vacacione vacacione)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(vacacione).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.idEmpleado = new SelectList(db.Empleados, "id", "codigoEmpleado", vacacione.idEmpleado);
+            return View(vacacione);
+        }
+
+        // GET: Vacaciones/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Vacacione vacacione = db.Vacaciones.Find(id);
+            if (vacacione == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vacacione);
+        }
+
+        // POST: Vacaciones/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Vacacione vacacione = db.Vacaciones.Find(id);
+            db.Vacaciones.Remove(vacacione);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
